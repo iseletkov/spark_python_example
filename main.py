7a201f72cfce4f7c8483665992fdced0c96d18be
iseletkov <iseletkov@gmail.com>
2023-12-17 23:44:24 +0500

    Пример линейной регрессии на Apache Spark.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.feature import VectorAssembler
+
+# https://bigdataschool.ru/blog/linear-regression-in-pyspark.html
+# https://ruslanmv.com/blog/Linear-Regression-with-Python-and-Spark
 
 host_spark = "localhost"
 host_db = "192.168.1.102"
@@ -20,6 +27,7 @@ def main():
         .appName("Test_db") \
         .getOrCreate()
 
+    # Загрузка исходных данных из СУБД
     courses = spark.read \
         .format("jdbc") \
         .option("url", f"jdbc:postgresql://{host_db}:{port_db}/{database}") \
@@ -27,11 +35,45 @@ def main():
         .option("user", user) \
         .option("password", password) \
         .option("driver", "org.postgresql.Driver") \
-        .load()
+        .load() \
+        .filter(col("num_code").isin([36, 156, 356, 840, 944, 978])) \
+        .withColumn("value", col("value").cast("double")) \
+        .groupBy("dttm") \
+        .pivot("num_code") \
+        .sum("value") \
+        .orderBy("dttm")
 
-    df = courses.toPandas()
-    df.to_excel("report.xlsx")
+    features = ["36", "156", "356", "944", "978"]
+    target = "840"
+
+    # Для обучения модели все исходные колонки собираются в одну,
+    # содержащую вектор из значений всех остальных колонок.
+    assembler = VectorAssembler(inputCols=features,
+                                outputCol='features')
+    courses_vectorized = assembler.transform(courses)
+
+    print("courses_vectorized:")
+    courses_vectorized.show()
+
+    # Деление на тестовую и обучающую выборки.
+    train, test = courses_vectorized.randomSplit([0.7, 0.3])
+
+    # Построение линейной регрессии
+    lr = LinearRegression(
+        featuresCol='features',
+        labelCol=target)
+
+    model = lr.fit(train, )
+
+    # Тестирование, расчёт метрик.
+    predicted = model.evaluate(test)
+    predicted.predictions.show()
+
+    # Вывод результата.
+    print(f"RMSE: {predicted.rootMeanSquaredError}")
+    print(f"MAE: {predicted.meanAbsoluteError}")
+
+    # model.write().overwrite().save("model")
 
 
 main()
-
